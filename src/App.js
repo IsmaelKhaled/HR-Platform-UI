f79395d4d448cfef753107fe4210a40dbe74ce35
Ismael Khaled <31388232+IsmaelKhaled@users.noreply.github.com>
2022-04-21 16:05:11 +0200

    Add basic router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.scss";
+import HomePage from "./components/HomePage/HomePage";
+import CreateInterview from "./components/Interviews/CreateInterview";
 
 function App() {
   return (
-    <div className="container">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/interviews/create/" element={<CreateInterview />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,10 +4,8 @@ class HomePage extends React.Component {
   render() {
     return (
       <>
-        <h1>
-          Home Page
-        </h1>
-        <p>This is out home page</p>
+        <h1>Home Page</h1>
+        <p>This is our home page</p>
       </>
     );
   }
